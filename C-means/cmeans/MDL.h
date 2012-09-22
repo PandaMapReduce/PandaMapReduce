13e2670ef3cc952da85a912f3ac33cd5f298627c
Hui Li <lihui@d010.(none)>
2012-09-22 13:54:41 -0400

    clean up panda repository

--- C-means/cmeans/MDL.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MDL_H__
-#define __MDL_H__ 1
-
-int* TabuSearch(float* matrix, char* inputFile);
-int* MDL(float* events, float* clusters, float* mdlTime, char* inputFile);
-int* MDLGPU(float* d_events, float* d_clusters, float* distanceMatrix, float* mdlTime, char* inputFile);
-
-#endif
--- pendelberry/fuzzymatrix.h
+++
@@ -1,9 +0,0 @@
-#include "clusteringutils.h"
-
-#define MAXITER 150
-
-void setCenters(Params* p); 
-void setScatterMatrices(Params* p);
-void computeGeneralFormula_eq31(Params* p, double beta, double tau, double THETA);
-void printCenters(Params* p);
-
