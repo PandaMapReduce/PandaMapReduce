7f10f7fb2102ef5a9bcf9fb277adeac0e9f6d176
Hui Li <lihui@d007.(none)>
2012-09-27 02:59:36 -0400

    tuned performance for cpu version of cmeans

--- PandaC-means/UserAPI.cu
+++ PandaC-means/UserAPI.cu
@@ -16,7 +16,90 @@
 #include "Panda.h"
 #include "UserAPI.h"
 
+//invoke cmeans_cpu_map_cpp compiled with g++
 void cpu_map(void *key, void *val, int keySize, int valSize, cpu_context *d_g_state, int map_task_idx){
+	
+	cmeans_cpu_map_cpp(key, val, keySize, valSize);
+	CPUEmitMapOutput(key, val, sizeof(KM_KEY_T), sizeof(KM_VAL_T), d_g_state, map_task_idx);
+
+}
+
+
+void cpu_map2(void *key, void *val, int keySize, int valSize, cpu_context *d_g_state, int map_task_idx){
+
+	KM_KEY_T* pKey = (KM_KEY_T*)key;
+	KM_VAL_T* pVal = (KM_VAL_T*)val;
+	
+	int dim = pKey->dim;
+	//int dim_4;
+	int K = pKey->K;
+	int start = pKey->start;
+	int end = pKey->end;
+	int index = pKey->local_map_id;
+	//TODO there could be problem here when running C-means with more than one GPU
+	//index = 0;
+
+	float *point	= (float*)(pVal->d_Points);
+	float *cluster	= (float*)(pVal->d_Clusters);
+
+	float * tempClusters = pVal->d_tempClusters+index*dim*K;
+	float * tempDenominators = pVal->d_tempDenominators+index*K;
+
+	float denominator = 0.0f;
+	float membershipValue = 0.0f;
+
+	float *distances = (float *)malloc(sizeof(float)*K);
+	float *numerator = (float *)malloc(sizeof(float)*K);
+	
+	for(int i=0; i<K; i++){
+		distances[i]=0.0f;
+		numerator[i]=0.0f;
+	}//for
+
+	//printf("map_task_id 0:%d thread_id:%d\n",map_task_idx,THREAD_ID);
+	for (int i=start; i<end; i++){
+		float *curPoint = (float*)(pVal->d_Points + i*dim);
+		for (int k = 0; k < K; ++k)
+		{
+			float* curCluster = (float*)(pVal->d_Clusters + k*dim);
+			distances[k] = 0.0;
+			//printf("dim:%d\n",dim);
+			//dim_4 = dim;
+			float delta = 0.0;	
+			
+			for (int j = 0; j < dim; ++j)
+			{
+				delta = curPoint[j]-curCluster[j];
+				distances[k] += (delta*delta);
+			}//for
+			
+			numerator[k] = powf(distances[k],2.0f/(2.0-1.0))+1e-30;
+			denominator  = denominator + 1.0f/(numerator[k]+1e-30);
+		}//for
+
+		for (int k = 0; k < K; ++k)
+		{
+			membershipValue = 1.0f/powf(numerator[k]*denominator,(float)2.0);
+			for(int d =0; d<dim; d++){
+				//float pt = curePoint[d].x;
+				tempClusters[k*dim+d] += (curPoint[d])*membershipValue;
+				
+			}
+			tempDenominators[k]+= membershipValue;
+		}//for 
+	}//for
+	//printf("map_task_id 1:%d\n",map_task_idx);
+	
+	free(distances);
+	free(numerator);
+	
+	//TODO
+	pKey->local_map_id = 0;
+	pKey->end = 0;
+	pKey->start = 0;
+	pKey->global_map_id = 0;
+	
+	CPUEmitMapOutput(key, val, sizeof(KM_KEY_T), sizeof(KM_VAL_T), d_g_state, map_task_idx);
 
 }//void
 
@@ -32,8 +115,7 @@ __device__ void gpu_map(void *key, void *val, int keySize, int valSize, gpu_cont
 	int end = pKey->end;
 	int index = pKey->local_map_id;
 	//TODO there could be problem here when running C-means with more than one GPU
-	//the index could be wrong if there are more than 2 GPUs
-	//index = 0;
+	index = 0;
 
 	float4 *point =(float4*)(pVal->d_Points);
 	float* cluster = (float*)(pVal->d_Clusters);
@@ -47,9 +129,7 @@ __device__ void gpu_map(void *key, void *val, int keySize, int valSize, gpu_cont
 	float *distances = (float *)malloc(sizeof(float)*K);
 	float *numerator = (float *)malloc(sizeof(float)*K);
 	
-	//float distances[10];
-	//float numerator[10];
-
+	
 	for(int i=0; i<K; i++){
 		distances[i]=0.0f;
 		numerator[i]=0.0f;
@@ -65,7 +145,6 @@ __device__ void gpu_map(void *key, void *val, int keySize, int valSize, gpu_cont
 			//printf("dim:%d\n",dim);
 			dim_4 = dim/4;
 			float delta = 0.0;	
-
 			
 			for (int j = 0; j < dim_4; ++j)
 			{
@@ -116,11 +195,11 @@ __device__ void gpu_map(void *key, void *val, int keySize, int valSize, gpu_cont
 	free(distances);
 	free(numerator);
 	
+	//TODO
 	pKey->local_map_id = 0;
 	pKey->end = 0;
 	pKey->start = 0;
 	pKey->global_map_id = 0;
-	
 	
 	GPUEmitMapOutput(key, val, sizeof(KM_KEY_T), sizeof(KM_VAL_T), d_g_state, map_task_idx);
 	
@@ -170,12 +249,64 @@ int cpu_compare(const void *key_a, int len_a, const void *key_b, int len_b)
 
 }
 
+
+void cpu_reduce(void *key, val_t* vals, int keySize, int valCount, cpu_context* d_g_state){
+	cmeans_cpu_reduce_cpp(key,  vals, keySize, valCount);
+	CPUEmitReduceOutput(key,vals,sizeof(KM_KEY_T), sizeof(KM_VAL_T), d_g_state);
+}
+
 //-------------------------------------------------------------------------
 //Reduce Function in this application
 //-------------------------------------------------------------------------
 
-void cpu_reduce(void *key, val_t* vals, int keySize, int valCount, cpu_context* d_g_state)
+void cpu_reduce2(void *key, val_t* vals, int keySize, int valCount, cpu_context* d_g_state)
 {
+
+		KM_KEY_T* pKey = (KM_KEY_T*)key;
+        int dim = pKey->dim;
+        int K = pKey->K;
+
+
+
+        float* myClusters = (float*) malloc(sizeof(float)*dim*K);
+        float* myDenominators = (float*) malloc(sizeof(float)*K);
+        memset(myClusters,0,sizeof(float)*dim*K);
+        memset(myDenominators,0,sizeof(float)*K);
+
+        float *tempClusters = NULL;
+        float *tempDenominators = NULL;
+		
+
+        for (int i = 0; i < valCount; i++)
+        {
+                int index = pKey->local_map_id;
+
+
+				KM_VAL_T* pVal = (KM_VAL_T*)(vals[i].val);
+                tempClusters = pVal->d_tempClusters + index*K*dim;
+                tempDenominators = pVal->d_tempDenominators+ index*K;
+                for (int k = 0; k< K; k++){
+                        for (int j = 0; j< dim; j++)
+                                myClusters[k*dim+j] += tempClusters[k*dim+j];
+                        myDenominators[k] += tempDenominators[k];
+                }//for
+        }//end for
+
+
+        for (int k = 0; k< K; k++){
+			for (int i = 0; i < dim; i++){
+						//printf("K:%d dim:%d myDenominators[i]:%f",K,dim,myDenominators[i]);
+                        myClusters[i] /= ((float)myDenominators[i]+0.0001);
+						//printf("%f ",myClusters[i]);
+			}//for
+			//printf("\n");
+        }//for
+
+		
+		free(myClusters);
+		free(myDenominators);
+
+		CPUEmitReduceOutput(key,vals,sizeof(KM_KEY_T), sizeof(KM_VAL_T), d_g_state);
 
 }
 
--- PandaC-means/main.cu
+++ PandaC-means/main.cu
@@ -47,25 +47,27 @@ int main(int argc, char** argv)
 	if (argc != 7)
 	{
 		printf("Panda C-means\n");
-		printf("usage: %s [numPt] [Dimensions] [numClusters] [numMapperPerGPU] [maxIter] [numGpus]\n", argv[0]);
-		exit(-1);
+		printf("usage: %s [numPt]  [gpu/cpu ratio] [numMapperPerCPU][numMapperPerGPU] [maxIter] [numGpus]\n", argv[0]);
+		exit(-1);//[Dimensions] [numClusters]
 	}//if
-
+	
 	//printf("start %s  %s  %s\n",argv[0],argv[1],argv[2]);
 	int numPt = atoi(argv[1]);
-	int dim = atoi(argv[2]);
-	int K = atoi(argv[3]);
-	int numMapper = atoi(argv[4]);
+	int dim = 100;//atoi(argv[2]);
+	int K = 10;//atoi(argv[3]);
+	float ratio = atof(argv[2]);
+	int numMapperCPU = atoi(argv[3]);
+	int numMapperGPU = atoi(argv[4]);
 	int maxIter = atoi(argv[5]);
 	int num_gpus = atoi(argv[6]);
-	double ratio = 0;
+	int num_cpus_groups = 1;
 	
 	panda_context *panda = CreatePandaContext();
 	panda->num_gpus = num_gpus;
-	panda->num_cpus_groups = 0;//num_cpus_groups;
+	panda->num_cpus_groups = num_cpus_groups;
 	panda->ratio = 0.0;
 
-	ShowLog("numPt:%d	dim:%d	K:%d	numMapper:%d	maxIter:%d",numPt,dim,K,numMapper,maxIter);
+	ShowLog("numPt:%d	dim:%d	K:%d	numMapperGPU:%d numMapperCPU:%d	maxIter:%d",numPt,dim,K,numMapperGPU,numMapperCPU,maxIter);
 	float* h_points = GenPointsFloat(numPt, dim);
 	float* h_cluster = GenInitCentersFloat(h_points, numPt, dim, K);
 	
@@ -73,23 +75,32 @@ int main(int argc, char** argv)
 	cudaGetDeviceCount(&numgpus);
 	if (num_gpus >= numgpus)		num_gpus = numgpus;
 			
-	pthread_t *no_threads = (pthread_t*)malloc(sizeof(pthread_t)*num_gpus);
-	thread_info_t *thread_info = (thread_info_t*)malloc(sizeof(thread_info_t)*num_gpus);
+	pthread_t *no_threads = (pthread_t*)malloc(sizeof(pthread_t)*(num_gpus+num_cpus_groups));
+	thread_info_t *thread_info = (thread_info_t*)malloc(sizeof(thread_info_t)*(num_gpus+num_cpus_groups));
 		
-	for (int i=0; i<num_gpus; i++){
+	int gpuWorkLoad = int(numPt * ratio);
+	int cpuWorkLoad = numPt - gpuWorkLoad;
+	if (num_gpus == 0) gpuWorkLoad = 0;
+	if (num_cpus_groups == 0) cpuWorkLoad = 0;
+
+
+	ShowLog("numPt:%d cpu workload:%d gpu workload:%d", numPt, cpuWorkLoad, gpuWorkLoad );
+
+	for (int dev_id=0; dev_id<num_gpus; dev_id++){
 
 		job_configuration *gpu_job_conf = CreateJobConf();
 		gpu_job_conf->num_gpus = num_gpus;
-		gpu_job_conf->num_mappers = numMapper;
+		gpu_job_conf->num_mappers = numMapperGPU;
 		gpu_job_conf->auto_tuning = false;
 		gpu_job_conf->ratio = (double)ratio;
 		gpu_job_conf->auto_tuning_sample_rate = -1;//sample_rate;
 		gpu_job_conf->iterative_support = false;
-		
-		int tid = i;		
+		//gpu_job_conf->local_combiner = true;
+
+		int tid = dev_id;		
 		float* d_points	=	NULL;
 		float* d_cluster =	NULL;
-		int* d_change	=	NULL;
+		//int* d_change	=	NULL;
 		int* d_clusterId =	NULL;
 		
 		float* d_tempClusters = NULL;
@@ -103,59 +114,58 @@ int main(int argc, char** argv)
 		checkCudaErrors(cudaMemset(d_clusterId, 0, numPt*sizeof(int)));
 		checkCudaErrors(cudaMalloc((void**)&d_cluster, K*dim*sizeof(int)));
 		checkCudaErrors(cudaMemcpy(d_cluster, h_cluster, K*dim*sizeof(int), cudaMemcpyHostToDevice));
-		checkCudaErrors(cudaMalloc((void**)&d_change, sizeof(int)));
-		checkCudaErrors(cudaMemset(d_change, 0, sizeof(int)));
+		//checkCudaErrors(cudaMalloc((void**)&d_change, sizeof(int)));
+		//checkCudaErrors(cudaMemset(d_change, 0, sizeof(int)));
+		checkCudaErrors(cudaMalloc((void**)&d_tempClusters,K*dim*numMapperGPU*sizeof(float)));
+		checkCudaErrors(cudaMemset(d_tempClusters, 0, sizeof(float)*K*dim*numMapperGPU));
+		checkCudaErrors(cudaMalloc((void**)&d_tempDenominators,numMapperGPU * K * sizeof(float)));
 		
-		checkCudaErrors(cudaMalloc((void**)&d_tempClusters,K*dim*numMapper*sizeof(float)));
-		checkCudaErrors(cudaMemset(d_tempClusters, 0, sizeof(float)*K*dim*numMapper));
-		checkCudaErrors(cudaMalloc((void**)&d_tempDenominators,numMapper * K * sizeof(float)));
+		checkCudaErrors(cudaMemset(d_tempDenominators, 0, sizeof(float)*K*numMapperGPU));
 		
-		checkCudaErrors(cudaMemset(d_tempDenominators, 0, sizeof(float)*K*numMapper));
-		
-		thread_info[i].tid = i;
-		//thread_info[i].num_gpus = num_gpus;
-		thread_info[i].device_type = GPU_ACC;
+		thread_info[dev_id].tid = dev_id;
+		//thread_info[dev_id].num_gpus = num_gpus;
+		thread_info[dev_id].device_type = GPU_ACC;
 		
 		cudaDeviceProp gpu_dev;
-		cudaGetDeviceProperties(&gpu_dev, i);
+		cudaGetDeviceProperties(&gpu_dev, dev_id);
 
-		ShowLog("Configure Device ID:%d: Device Name:%s", i, gpu_dev.name);
-		thread_info[i].device_name = gpu_dev.name;
+		ShowLog("Configure Device ID:%d: Device Name:%s", dev_id, gpu_dev.name);
+		thread_info[dev_id].device_name = gpu_dev.name;
 				
 		KM_VAL_T val;
 		val.ptrPoints = (int *)d_points;
 		val.ptrClusters = (int *)d_cluster;
 		val.d_Points = d_points;
 		val.d_Clusters = d_cluster;
-		val.ptrChange = d_change;
+		//val.ptrChange = d_change;
 		
 		KM_KEY_T key;
 		key.dim = dim;
 		key.K = K;
 		key.ptrClusterId = d_clusterId;
 		
-		int numPtPerGPU = numPt/num_gpus;
-		int start = i*numPtPerGPU;
+		int numPtPerGPU = gpuWorkLoad/num_gpus;
+		int start = dev_id*numPtPerGPU;
 		int end = start+numPtPerGPU;
-		if (i==num_gpus-1)
-			end = numPt;
+		if (dev_id==num_gpus-1)
+			end = gpuWorkLoad;
 		
-		int numPtPerMap = (end-start)/numMapper;
-		ShowLog("numPtPerMap:%d startPt:%d  endPt:%d numPt:%d",numPtPerMap,start,end,numPt);
+		int numPtPerMap = (end-start)/numMapperGPU;
+		ShowLog("GPU numPtPerMap:%d startPt:%d  endPt:%d numPt:%d gpuWorkLoad:%d",numPtPerMap,start,end,numPt,gpuWorkLoad);
 
 		int start_i,end_i;
 		start_i = start;
-		for (int j = 0; j < numMapper; j++)
+		for (int j = 0; j < numMapperGPU; j++)
 		{	
 			end_i = start_i + numPtPerMap;
-			if (i<(end-start)%numMapper)
+			if (dev_id<(end-start)%numMapperGPU)
 				end_i++;
 			
 			//ShowLog("start_i:%d, start_j:%d",start_i,end_i);
 			//key.point_id = start_i;
 			key.start = start_i;
 			key.end = end_i;
-			key.global_map_id = i*numMapper+j;
+			key.global_map_id = dev_id*numMapperGPU+j;
 			key.local_map_id = j;
 
 			val.d_Points = d_points;
@@ -165,12 +175,107 @@ int main(int argc, char** argv)
 			AddPandaTask(gpu_job_conf, &key, &val, sizeof(KM_KEY_T), sizeof(KM_VAL_T));
 			start_i = end_i;
 		}//for
-
-		thread_info[i].job_conf = gpu_job_conf;
-		thread_info[i].device_type = GPU_ACC;
+		thread_info[dev_id].job_conf = gpu_job_conf;
+		thread_info[dev_id].device_type = GPU_ACC;
 	}//for
-	double t1 = PandaTimer();
+
+	for (int dev_id=num_gpus; dev_id < num_gpus+num_cpus_groups; dev_id++){
+
+		job_configuration *cpu_job_conf = CreateJobConf();
+		cpu_job_conf->num_cpus_groups = num_cpus_groups;
+		cpu_job_conf->num_cpus_cores = getCPUCoresNum();
+		//cpu_job_conf->local_combiner = true;
+		cpu_job_conf->ratio = (double)ratio;
+		cpu_job_conf->auto_tuning_sample_rate = -1;
+		cpu_job_conf->iterative_support = false;
+		cpu_job_conf->local_combiner = false;
+
+		int tid = dev_id;		
+		float* d_points	=	NULL;
+		float* d_cluster =	NULL;
+		int* d_change	=	NULL;
+		int* d_clusterId =	NULL;
 		
+		float* h_tempClusters = NULL;
+		float* h_tempDenominators = NULL;
+
+		h_tempClusters = (float *)malloc(K*dim*numMapperGPU*sizeof(float));
+		h_tempDenominators = (float *)malloc(numMapperGPU*K*sizeof(float));
+
+		/*
+		checkCudaErrors(cudaSetDevice(tid));
+		checkCudaErrors(cudaMalloc((void**)&d_points, numPt*dim*sizeof(int)));
+		checkCudaErrors(cudaMemcpy(d_points, h_points, numPt*dim*sizeof(int), cudaMemcpyHostToDevice));
+		checkCudaErrors(cudaMalloc((void**)&d_clusterId, numPt*sizeof(int)));
+		checkCudaErrors(cudaMemset(d_clusterId, 0, numPt*sizeof(int)));
+		checkCudaErrors(cudaMalloc((void**)&d_cluster, K*dim*sizeof(int)));
+		checkCudaErrors(cudaMemcpy(d_cluster, h_cluster, K*dim*sizeof(int), cudaMemcpyHostToDevice));
+		//checkCudaErrors(cudaMalloc((void**)&d_change, sizeof(int)));
+		//checkCudaErrors(cudaMemset(d_change, 0, sizeof(int)));
+		checkCudaErrors(cudaMalloc((void**)&d_tempClusters,K*dim*numMapper*sizeof(float)));
+		checkCudaErrors(cudaMemset(d_tempClusters, 0, sizeof(float)*K*dim*numMapper));
+		checkCudaErrors(cudaMalloc((void**)&d_tempDenominators,numMapper * K * sizeof(float)));
+		checkCudaErrors(cudaMemset(d_tempDenominators, 0, sizeof(float)*K*numMapper));
+		cudaGetDeviceProperties(&gpu_dev, i);
+		ShowLog("Configure Device ID:%d: Device Name:%s", i, gpu_dev.name);
+		*/		
+
+		thread_info[dev_id].device_name = "CPU";
+				
+		KM_VAL_T val;
+		val.ptrPoints = (int *)h_points;
+		val.ptrClusters = (int *)h_cluster;
+		val.d_Points = h_points;
+		val.d_Clusters = h_cluster;
+		//val.ptrChange = d_change;
+		
+		KM_KEY_T key;
+		key.dim = dim;
+		key.K = K;
+		int * h_clusterId = (int*)malloc(sizeof(int)*numPt);
+		key.ptrClusterId = h_clusterId;
+		
+		int numPtPerCPU = cpuWorkLoad/num_cpus_groups;
+		int start = cpuWorkLoad+ (dev_id-num_gpus)*numPtPerCPU;
+		int end = start+numPtPerCPU;
+
+		if (dev_id==num_gpus+num_cpus_groups-1)
+			end = numPt;
+		
+		int numPtPerMap = (end-start)/numMapperCPU;
+		ShowLog("CPU numPtPerMap:%d startPt:%d  endPt:%d numPt:%d cpuWorkLoad:%d",numPtPerMap,start,end,numPt,cpuWorkLoad);
+
+		int start_i, end_i;
+		start_i = start;
+		for (int j = 0; j < numMapperCPU; j++)
+		{	
+			end_i = start_i + numPtPerMap;
+			if (dev_id<(end-start)%numMapperCPU)
+				end_i++;
+			
+			//ShowLog("start_i:%d, start_j:%d",start_i,end_i);
+			//key.point_id = start_i;
+			key.start = start_i;
+			key.end = end_i;
+			key.global_map_id = dev_id*numMapperCPU+j;
+			key.local_map_id = j;
+
+			//val.d_Points = h_points;
+			val.d_tempDenominators = h_tempDenominators;
+			val.d_tempClusters = h_tempClusters;
+
+			AddPandaTask(cpu_job_conf, &key, &val, sizeof(KM_KEY_T), sizeof(KM_VAL_T));
+			start_i = end_i;
+		}//for
+		
+		thread_info[dev_id].job_conf = cpu_job_conf;
+		thread_info[dev_id].device_type = CPU_ACC;
+		thread_info[dev_id].tid = dev_id;
+		
+	}//for
+
+
+	double t1 = PandaTimer();
 	int iter = 0;
 	while (iter<maxIter)
 	{
@@ -202,7 +307,7 @@ int main(int argc, char** argv)
 
 	double t2 = PandaTimer();
 	ShowLog("Panda C-means take %f sec", t2-t1);
-	DoLog2Disk("== Panda C-means numPt:%d	dim:%d	K:%d	numMapper:%d	maxIter:%d take %f sec",numPt,dim,K,numMapper,maxIter, t2-t1);
+	DoLog2Disk("== Panda C-means numPt:%d	dim:%d	K:%d	numMapperGPU:%d	numMapperCPU:%d maxIter:%d take %f sec",numPt,dim,K,numMapperGPU,numMapperCPU,maxIter, t2-t1);
 
 	return 0;
 }//		
